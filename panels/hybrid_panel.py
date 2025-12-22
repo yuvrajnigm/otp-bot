a92f0e7b9ad8db6fdc6cea633c8e1b23a063b298
yuvrajnigm <yuvrajnigam79@gmail.com>
2025-12-22 10:11:36 +0530

    Modify fetch_any to include panel_id parameter

--- panels/hybrid_panel.py
+++ panels/hybrid_panel.py
@@ -1,8 +1,8 @@
 from panels.sms_panel import fetch_sms
 from panels.call_panel import fetch_call
 
-async def fetch_any(panel):
+async def fetch_any(panel_id, panel):
     try:
-        return await fetch_sms(panel)
+        return await fetch_sms(panel_id, panel)
     except:
-        return await fetch_call(panel)
+        return await fetch_call(panel_id, panel)
